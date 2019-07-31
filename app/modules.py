09b8f10e46a2b055cee556155f43d0a0a6351d12
Christoffel <ccolombijn@gmail.com>
2019-07-31 19:27:04 +0200

    Updated modules

--- app/modules.py
+++ app/modules.py
@@ -1,14 +1,18 @@
 from app import app
 from app import config
 import importlib.util
+obj = {}
+def add(prop,module):
+    obj[prop] = module
+def run(prop):
+    obj[prop].default()
 def get():
     modules = config.get()['modules']
     i = 0
     while(i < len(modules)):
         spec = importlib.util.spec_from_file_location('default', '''modules/''' + modules[i] + '''.py''')
         module = importlib.util.module_from_spec(spec)
-        print(module)
         spec.loader.exec_module(module)
-        module.default()
+        add(modules[i],module)
+        print(module)
         i+=1
-        
--- modules/dashboard.py
+++ modules/dashboard.py
@@ -1,3 +1,4 @@
 from app import app
+name = 'Dashboard'
 def default():
     print('This is dashboard')
